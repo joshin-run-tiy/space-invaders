c49310666008846d6aa02ca1a5aed03794dc247e
joshin-run <jahmsc@gmail.com>
2017-03-28 15:00:26 -0600

    Feat (script) Add players; Now how small rect instead of large one

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,8 +2,9 @@
   var Game = function(canvasId) {
     var canvas = document.getElementById(canvasId);
     var screen = canvas.getContext('2d');
-    var gameSize = { x: canvas.wigth, y: canvas.height }
-    console.log('hello');
+    var gameSize = { x: canvas.width, y: canvas.height }
+
+    this.bodies = [new Player(this, gameSize)];
 
     var self = this;
     var tick = function() {
@@ -17,22 +18,36 @@
 
   Game.prototype = {
     update: function() {
-      console.log('hello');
+
     },
 
-    // draw: function() {
       draw: function(screen, gameSize) {
-        screen.fillRect(30, 30, 40, 40);
+        // screen.fillRect(30, 30, 40, 40);
+        for (var i = 0; i < this.bodies.length; i++) {
+          drawRect(screen, this.bodies[i]);
+        }
       }
+    };
+
+  var Player = function(game, gameSize) {
+    this.game = game;
+    this.size = { x: 15, y: 15 };
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+  };
+
+  Player.prototype = {
+    update: function() {
+
     }
-  // };
+  }
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
+  };
 
   window.onload = function() {
     new Game("screen");
   };
 })();
-
-
-
-
-console.log("hi");
