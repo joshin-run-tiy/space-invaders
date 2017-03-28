f3a49780ea67996409496f89550d141384043cf8
joshin-run <jahmsc@gmail.com>
2017-03-28 15:44:53 -0600

    Feat (main) Add left/right movement for rectangle

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,11 +18,15 @@
 
   Game.prototype = {
     update: function() {
-
+      // console.log("heya21");
+      for (var i = 0; i < this.bodies.length; i++) {
+        this.bodies[i].update();
+      }
     },
 
       draw: function(screen, gameSize) {
         // screen.fillRect(30, 30, 40, 40);
+        // screen.clearRect(0, 0, gameSize.x, gameSize.y)
         for (var i = 0; i < this.bodies.length; i++) {
           drawRect(screen, this.bodies[i]);
         }
@@ -33,19 +37,45 @@
     this.game = game;
     this.size = { x: 15, y: 15 };
     this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+    this.keyboarder = new Keyboarder();
   };
 
   Player.prototype = {
     update: function() {
+      console.log("haya42!");
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -=2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x +=2;
 
+        }
     }
-  }
+  };
 
   var drawRect = function(screen, body) {
     screen.fillRect(body.center.x - body.size.x / 2,
                     body.center.y - body.size.y / 2,
                     body.size.x, body.size.y);
   };
+
+  var Keyboarder = function() {
+    var keyState = {};
+
+    window.onkeydown = function(e) {
+      keyState[e.keyCode] = true;
+    };
+
+    window.onkeyup = function(e) {
+      keyState[e.keyCode] = false;
+    };
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32}
+
+  }
 
   window.onload = function() {
     new Game("screen");
